0fa962ff793cfb34d5ea17e1cccc77bd72afabb1
zafchiel <bialakropla@proton.me>
2024-05-21 21:19:49 +0200

    cart items count

--- src/components/Koszyk.js
+++ src/components/Koszyk.js
@@ -10,6 +10,9 @@ export default function Koszyk() {
       <button onClick={() => setIsOpen((prev) => !prev)} className="bg-primary px-4 py-2 rounded-md">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
       </button>
+      <div className="absolute -bottom-2 -right-2 font-bold border-white border rounded-full bg-accent py-1 px-2 text-xs">
+        {cart.length}
+      </div>
 
       {isOpen && (
         <div className="absolute -left-1/2 p-2 rounded-md bg-primary w-max">
