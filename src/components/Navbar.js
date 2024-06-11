7b63cfc76021bf37bf9ee623bc91062012c683cf
zafchiel <bialakropla@proton.me>
2024-06-11 20:22:39 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,42 +5,42 @@ import Koszyk from "./Koszyk";
 const Navbar = () => {
   return (
     <nav className="flex fixed top-0 inset-x-0 bg-white justify-around py-4 z-50 items-center">
-          <img src={logo} className="nav-logo hidden md:block" alt="backroads" />
-          {/* <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars"></i>
-          </button> */}
+      <img src={logo} className="nav-logo hidden md:block" alt="backroads" />
 
-        <ul className="flex gap-4" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <ul className="flex gap-4" id="nav-links">
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a
+                href={link.href}
+                className="nav-link md:text-xl hover:text-[#2cb3d3] transition-all duration-500 capitalize"
+              >
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
 
-        <Koszyk />
+      <Koszyk />
 
-        <ul className="hidden sm:block">
-          {socialLinks.map((link) => {
-            const { id, href, icon } = link;
-            return (
-              <li key={id}>
-                <a
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="nav-icon"
-                >
-                  <i className={icon}></i>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <ul className="">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                className="nav-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
   );
 };
